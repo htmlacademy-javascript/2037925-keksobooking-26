5ee52d1f0753032b62606ec8ef75c4b32e26cbdb
Ibrahim1944 <danetov88@gmail.com>
2022-07-11 12:17:39 +0300

    Валидация

--- js/form-activation.js
+++ js/form-activation.js
@@ -1,7 +1,15 @@
+const offerForm = document.querySelector('.ad-form');
+const offerFormElements = offerForm.children;
 const mapFilterForm = document.querySelector('.map__filters');
 const mapFilterFormElements = mapFilterForm.children;
 
 function blockForm () {
+  offerForm.classList.add('ad-form--disabled');
+
+  for (const element of offerFormElements) {
+    element.disabled = true;
+  }
+
   mapFilterForm.classList.add('map__filters--disabled');
 
   for (const element of mapFilterFormElements){
@@ -13,6 +21,12 @@ blockForm();
 
 
 function unlockForm () {
+  offerForm.classList.remove('ad-form--disabled');
+
+  for (const element of offerFormElements) {
+    element.disabled = false;
+  }
+
   mapFilterForm.classList.remove('map__filters--disabled');
 
   for (const element of mapFilterFormElements){
