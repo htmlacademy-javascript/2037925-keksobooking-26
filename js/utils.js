ebe88cc685793999dba7c171c580ecb6046e3f5f
Ibrahim1944 <danetov88@gmail.com>
2022-07-30 14:59:26 +0300

    Финальные правки

--- js/utils.js
+++ js/utils.js
@@ -4,34 +4,22 @@ const DEFAULT_LAT = 35.6895;
 const DEFAULT_LNG = 139.692;
 const AVATAR_DEFAULT = 'img/muffin-grey.svg';
 
-const typeOfHousingPrice = {
-  palace: 10000,
-  flat: 1000,
-  house: 5000,
-  bungalow: 0,
-  hotel: 3000
-};
-
 const addFormElement = document.querySelector('.ad-form');
 const fullResetButton = document.querySelector('.ad-form__reset');
 const mapFilterForm = document.querySelector('.map__filters');
 const addressField = document.querySelector('#address');
-const price = addFormElement.querySelector('#price');
 const priceSlider = document.querySelector('.ad-form__slider');
 const previewPhoto = document.querySelector('.ad-form__photo');
 const previewAvatar = document.querySelector('.ad-form-header__avatar');
-const typeOfHousing = document.querySelector('#type');
 
 const resetForm = (evt) => {
   evt.preventDefault();
-
   addFormElement.reset();
   mapFilterForm.reset();
   resetMap();
   addressField.value = `${DEFAULT_LAT}, ${DEFAULT_LNG}`;
   previewAvatar.src = AVATAR_DEFAULT;
   previewPhoto.innerHTML = '';
-  price.placeholder = typeOfHousingPrice[typeOfHousing.value];
   priceSlider.noUiSlider.reset();
 };
 
